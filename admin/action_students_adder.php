8797cbaf4ba3100e85c68031e788bc7646cb91f6
Dulakshan Wijesundara <dulakshanwije@gmail.com>
2023-06-04 07:42:43 +0530

    Minor Changes

--- admin/action_students_adder.php
+++ admin/action_students_adder.php
@@ -22,7 +22,7 @@ if(isset($_POST['index']) && isset($_POST['name']) && isset($_POST['nic_number']
     $combination = validate($_POST['combination']);
     $batch_id = validate($_POST['batch_id']);
 
-    $student_add_query = "INSERT INTO student_table (student_index_number, student_nic_number, student_initials_name,student_batch_id, student_base_comb,student_password, student_first_time_login) VALUES ('$index', '$nic_number', '$name', '$batch_id', '$combination','$nic_number' ,'1')";
+    $student_add_query = "INSERT INTO student_table (student_index_number, student_nic_number, student_initials_name,student_batch_id, student_base_comb,student_password, student_first_time_login) VALUES ($index, '$nic_number', '$name', $batch_id, $combination,'$nic_number' ,'1')";
 
     echo $student_add_query;
 
