705a1e3b346d892dbe3fa20ec1f52651c7bcb686
Dulakshan Wijesundara <dulakshanwije@gmail.com>
2023-07-09 02:08:49 +0530

    New UI set added.
    New UI set were combined with back end. All the HTML and CSS files can find on UIs folder.

--- third_year_selection/general/gn_form_adder.php
+++ third_year_selection/general/gn_form_adder.php
@@ -1,26 +1,26 @@
 <?php
 
-    require "../config/config.php";
+    require "../../config/config.php";
     
     $current_index = $_SESSION['current_student_index'];
     $gn_choice = $_SESSION['gn_choice'];
     $gn_comb = $_SESSION['gn_comb'];
 
-    $check_query = "SELECT student_id FROM selection_form_table WHERE student_id = $current_index";
+    $check_query = "SELECT student_index_number FROM selection_form_table WHERE student_index_number = $current_index";
 
     if($q_return = mysqli_query($conn, $check_query)){
         if(mysqli_num_rows($q_return) > 0){
-            $query = "UPDATE selection_form_table SET gn_choice_$gn_choice = '$gn_comb' WHERE student_id = $current_index"; 
+            $query = "UPDATE selection_form_table SET gn_choice_$gn_choice = '$gn_comb' WHERE student_index_number = $current_index"; 
         }
         else{
-            $query = "INSERT INTO selection_form_table (gn_choice_$gn_choice, student_id) VALUES ('$gn_comb',$current_index)";
+            $query = "INSERT INTO selection_form_table (gn_choice_$gn_choice, student_index_number) VALUES ('$gn_comb',$current_index)";
         }
     }
 
     if($q_return = mysqli_query($conn, $query)){
         $_SESSION['gn_choice'] ="";
         $_SESSION['gn_comb'] ="";
-        header("Location:sub_view.php");
+        header("Location:../sub_view.php");
     }
 
 ?>
--- third_year_selection/joint_major/jm_form_adder.php
+++ third_year_selection/joint_major/jm_form_adder.php
@@ -5,16 +5,16 @@
     $jm_choice = $_SESSION['jm_choice'];
     $jm_comb = $_SESSION['jm_comb'];
 
-    $check_query = "SELECT student_id FROM selection_form_table WHERE student_id = $current_index";
+    $check_query = "SELECT student_index_number FROM selection_form_table WHERE student_index_number = $current_index";
 
     $query;
 
     if($q_return = mysqli_query($conn, $check_query)){
         if(mysqli_num_rows($q_return) > 0){
-            $query = "UPDATE selection_form_table SET jm_choice_$jm_choice = '$jm_comb' WHERE student_id = $current_index"; 
+            $query = "UPDATE selection_form_table SET jm_choice_$jm_choice = '$jm_comb' WHERE student_index_number = $current_index"; 
         }
         else{
-            $query = "INSERT INTO selection_form_table (jm_choice_$jm_choice, student_id) VALUES ('$jm_comb',$current_index)";
+            $query = "INSERT INTO selection_form_table (jm_choice_$jm_choice, student_index_number) VALUES ('$jm_comb',$current_index)";
         }
     }
 
@@ -23,7 +23,7 @@
     if($q_return = mysqli_query($conn, $query)){
         $_SESSION['jm_choice'] ="";
         $_SESSION['jm_comb'] ="";
-        header("Location:sub_view.php");
+        header("Location:../sub_view.php");
     }
 
 ?>
