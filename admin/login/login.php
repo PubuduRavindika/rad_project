f9a4dc65b0804135a89afd88072e63d0a71612c1
Dulakshan Wijesundara <dulakshanwije@gmail.com>
2023-07-11 15:34:53 +0530

    Admin login UI added.

--- admin/login/login.php
+++ admin/login/login.php
@@ -14,30 +14,54 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
-
-    <!-- Stylesheet -->
-    <link rel="stylesheet" href="../../assets/style.css">
+    <link rel="stylesheet" href="style.css"/>
+    <title>Admin Login Page</title>
 </head>
 <body>
-<section class="login_section">
-        <div class="login_box">
-            <h1>Admin Login</h1>
-            <?php
-            if(isset($_GET["result"])){
-                echo "<p>".$_GET['result']."</p>";
-            }
-            ?>
-            <form action="action_admin_login.php" method="POST">
-                <label for="username">Username: </label>
-                <input type="text" id="username" name="username">
-                <label for="password">Password: </label>
-                <input type="password" id="password" name="password">
-                <div>
-                    <input type="submit" value="Sign In">
+    <div class="container">
+        <div class="login-left">
+            <div class="login-header">
+                <h1 class="h1-header">Welcome to Students Registration - Admin Login</h1>
+                <!-- <h1 class="h1-header">Admin Login</h1> -->
+                <p class="p-header">E-Registration System - wayamba University</p>
+            </div>
+            <form class="login-form" action="action_admin_login.php" method="POST">
+                <div class="login-form-content">
+                    <?php
+                        if(isset($_GET["result"])){
+                            echo "<p class = 'error'>".$_GET['result']."</p>";
+                        }
+                    ?>
+                    <div class="form-item">
+                        <label for="email" class="email-label">Enter Username:</label>
+                        <input type="text" id="username" name= "username" class="email">                        
+                    </div>
+                    <div class="form-item">
+                        <label for="password" class="password-label">Enter Password</label>
+                        <input type="pssaword" id="password" class="password" name="password">                        
+                    </div>
+                    <div class="form-item">
+                        <a href="#" class="a-fogetpassword">Forgotten Password?</a>
+                    </div>
+                    <div class="form-item">
+                        <div class="checkbox">
+                            <input type="checkbox" id="rememberMeCheckbox" class="rememberMeCheckbox" checked>
+                            <label for="checkbox" id="checkboxLable" class="checkboxLable">Remember me</label>
+                        </div>
+                    </div>
+                    <button type="submit">LOG IN</button>
                 </div>
+                <!-- <div class="login-form-footer">
+                    <a href="#" class="a">
+                        <img width="30" src="google.png" alt="">
+                        Google Login
+                    </a>
+                </div> -->
             </form>
         </div>
-    </section>
+        <div class="login-right">
+            <img src="admin.svg" width="80%">
+        </div>
+    </div>
 </body>
 </html>
