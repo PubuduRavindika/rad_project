691d13f6afa82ca62e50ff08f0c7c2e0e7e0809c
Dulakshan Wijesundara <dulakshanwije@gmail.com>
2023-07-11 23:24:23 +0530

    First checkpoint.

---
+++ staff/dashboard/dashboard.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body class="applocation-body">
+    <div class="nav_container">
+        <div class="nav_title">
+            Student Registration System | WUSL
+        </div>
+        <div class="nav_details">
+            <span>
+                <p>W.M.D.I. Wijesundara</p>
+                <div class="nav_btn_holder"><span>192153</span><button>LOGOUT</button></div>
+            </span>
+            <img src="profile-2.png" alt="">
+        </div>
+    </div>
+
+    <div class="main-container">
+        <div class="headder">Third Year Selection Filled Forms</div>
+        <div class="application">
+            <img class="app-img" src="profile-2.png" alt="">
+            <div class="details">192153</div>
+            <div class="details">Wijesundara W.M.D.I.</div>
+            <div class="details">3rd Year Selection</div>
+            <button class="view-btn" onclick="location.href='../application_view/index.html'">View</button>
+        </div>
+
+        <!-- <div class="application">
+            <img class="app-img" src="profile-2.png" alt="">
+            <div class="details">192153</div>
+            <div class="details">W.M.D.I. Wijesundara</div>
+            <div class="details">Lorem ipsum dolor sit amet.</div>
+            <button class="view-btn">View</button>
+        </div> -->
+    </div>
+    
+</body>
+</html>
--- staff/login/login.php
+++ staff/login/login.php
@@ -1,49 +1,60 @@
 <?php
 
-require "../config/config.php";
+    require ("../../config/config.php");
 
-if (isset($_SESSION["current_student_id"])) {
-    header("Location:../dashboard/dashboard.php");
-}
+    
+    if(isset($_POST['submit'])){
+        $nic = $_POST['nic'];
+        $password = $_POST['password'];
+        
+        $sql = "SELECT admin_nic, admin_password FROM admin_table WHERE admin_nic = '$nic'";
+
+        $result = mysqli_query($conn, $sql);
+
+        while($row = mysqli_fetch_assoc($result)){
+            if($row['admin_password'] == $password){
+                header("location:../dashboard/dashboard.php");
+                echo "Done";
+            }
+        }
+    }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css" />
-    <title>Login Page</title>
+    <link rel="stylesheet" href="style.css"/>
+    <title>Admin Login Page</title>
 </head>
-
 <body>
     <div class="container">
         <div class="login-left">
             <div class="login-header">
-                <h1 class="h1-header">Welcome to Students Registration</h1>
-                <p class="p-header">E-Registration System - WUSL</p>
+                <h1 class="h1-header">Course Registration System</h1>
+                <!-- <h1 class="h1-header">Admin Login</h1> -->
+                <p class="p-header">Staff Login - Wayamba University</p>
             </div>
-            <form class="login-form" action="login_validation.php" method="post">
+            <form class="login-form" action="" method="POST">
                 <div class="login-form-content">
                     <?php
-                    if (isset($_GET["result"])) {
-                        echo "<p class = 'error'>* " . $_GET['result'] . "</p>";
-                    }
+                        if(isset($_GET["result"])){
+                            echo "<p class = 'error'>".$_GET['result']."</p>";
+                        }
                     ?>
                     <div class="form-item">
-                        <label for="index" class="email-label">Enter Index</label>
-                        <input name="index" type="text" id="index" class="email">
+                        <label for="email" class="email-label">Enter NIC:</label>
+                        <input type="text" id="username" name= "nic" class="email">                        
                     </div>
                     <div class="form-item">
                         <label for="password" class="password-label">Enter Password</label>
-                        <input type="password" id="password" class="password" name="password">
+                        <input type="password" id="password" class="password" name="password">                        
                     </div>
                     <div class="form-item">
                         <!-- <a href="#" class="a-fogetpassword">Forgotten Password?</a> -->
-                        <a href="../staff/login/login.php" class="a-fogetpassword">Login as Staff Member</a>
                     </div>
                     <div class="form-item">
                         <div class="checkbox">
@@ -51,7 +62,7 @@ if (isset($_SESSION["current_student_id"])) {
                             <label for="checkbox" id="checkboxLable" class="checkboxLable">Remember me</label>
                         </div>
                     </div>
-                    <button type="submit">LOG IN</button>
+                    <button type="submit" name = "submit">LOG IN</button>
                 </div>
                 <!-- <div class="login-form-footer">
                     <a href="#" class="a">
@@ -66,5 +77,4 @@ if (isset($_SESSION["current_student_id"])) {
         </div>
     </div>
 </body>
-
 </html>
